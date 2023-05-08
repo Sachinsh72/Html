440cffc4e98d10e9aff825e08d44608addcd178e
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-05-08 23:49:54 +0530

    Tailwind

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html"],
+  content: ["../dist/index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
 
